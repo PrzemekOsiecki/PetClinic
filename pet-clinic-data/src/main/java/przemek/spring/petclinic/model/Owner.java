bb5ae38c8c795a195a5752b0914ce9c5d3221d01
Przemek Osiecki <przemyslaw.osiecki@gmail.com>
2019-01-01 21:37:02 +0100

    Change Owner entity for future use of DB

--- pet-clinic-data/src/main/java/przemek/spring/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/przemek/spring/petclinic/model/Owner.java
@@ -1,9 +1,11 @@
 package przemek.spring.petclinic.model;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.HashSet;
 import java.util.Set;
 
 @Setter
@@ -13,20 +15,7 @@ import java.util.Set;
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
-
-    @Builder
-    public Owner(Long id, String firstName, String lastName, String address, String city,
-                 String telephone, Set<Pet> pets) {
-        super(id, firstName, lastName);
-        this.address = address;
-        this.city = city;
-        this.telephone = telephone;
-
-        if(pets != null) {
-            this.pets = pets;
-        }
-    }
-
+    
     @Column(name = "address")
     private String address;
 
@@ -37,31 +26,6 @@ public class Owner extends Person {
     private String telephone;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
-    private Set<Pet> pets = new HashSet<>();
-
-
-    /**
-     * Return the Pet with the given name, or null if none found for this Owner.
-     */
-    public Pet getPet(String name) {
-        return getPet(name, false);
-    }
-
-    /**
-     * Return the Pet with the given name, or null if none found for this Owner.
-     */
-    public Pet getPet(String name, boolean ignoreNew) {
-        name = name.toLowerCase();
-        for (Pet pet : pets) {
-            if (!ignoreNew || !pet.isNew()) {
-                String compName = pet.getName();
-                compName = compName.toLowerCase();
-                if (compName.equals(name)) {
-                    return pet;
-                }
-            }
-        }
-        return null;
-    }
+    private Set<Pet> pets;
 
 }
