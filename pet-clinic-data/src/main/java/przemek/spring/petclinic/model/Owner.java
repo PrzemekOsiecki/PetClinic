145452eabba03f78c8e765364de1bdb5d1172e3b
Przemek Osiecki <przemyslaw.osiecki@gmail.com>
2019-03-24 11:05:00 +0100

    gix circle CI build

--- pet-clinic-data/src/main/java/przemek/spring/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/przemek/spring/petclinic/model/Owner.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Setter
-@Getter
 @NoArgsConstructor
 @ToString
 @Entity
@@ -30,15 +29,38 @@ public class Owner extends Person {
         this.pets = pets;
     }
 
+    @Getter
     @Column(name = "address")
     private String address;
 
+    @Getter
     @Column(name = "city")
     private String city;
 
+    @Getter
     @Column(name = "telephone")
     private String telephone;
 
+    @Getter
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
+    }
+
 }
