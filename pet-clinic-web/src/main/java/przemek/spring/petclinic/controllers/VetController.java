e360e057d4d67ac5e2450f6b0e592fabe35ddd8a
Przemek Osiecki <przemyslaw.osiecki@gmail.com>
2019-03-24 11:22:02 +0100

    Displaying list of veterinarians and rest end-point to fetch veterinarians data in json format

--- pet-clinic-web/src/main/java/przemek/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/przemek/spring/petclinic/controllers/VetController.java
@@ -3,8 +3,13 @@ package przemek.spring.petclinic.controllers;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import przemek.spring.petclinic.model.Vet;
 import przemek.spring.petclinic.service.VetService;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("vets")
@@ -15,9 +20,13 @@ public class VetController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     String index(Model model) {
-
         model.addAttribute("vets", vetService.findAll());
-
         return "vets/index";
+    }
+
+    @GetMapping("/api")
+    @ResponseBody
+    Set<Vet> getVetJsonData() {
+        return vetService.findAll();
     }
 }
